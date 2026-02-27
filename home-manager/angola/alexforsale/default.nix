d4dcc4f09a2c7ee888437a4aa25454bfe8a77ef8
alexforsale <alexforsale@yahoo.com>
2026-02-27 22:13:53 +0700

    home-manager/angola/alexforsale: remove duplicate

--- home-manager/angola/alexforsale/default.nix
+++ home-manager/angola/alexforsale/default.nix
@@ -20,38 +20,10 @@
     stateVersion = "25.11";
     packages = with pkgs; [
       pass-git-helper
-      playerctl
-      mpc
-      mpd-notification
       hunspell
       hunspellDicts.en_US-large
       hunspellDicts.id_ID
       gruvbox-gtk-theme
     ];
-  };
-
-  xdg = {
-    enable = true;
-    userDirs = {
-      enable = true;
-      createDirectories = true;
-      music = "${config.home.homeDirectory}/Music";
-      videos = "${config.home.homeDirectory}/Videos";
-      pictures = "${config.home.homeDirectory}/Pictures";
-      templates = "${config.home.homeDirectory}/Templates";
-      download = "${config.home.homeDirectory}/Downloads";
-      documents = "${config.home.homeDirectory}/Documents";
-      desktop = "${config.home.homeDirectory}/Desktop";
-      publicShare = "${config.home.homeDirectory}/Public";
-      extraConfig = {
-        XDG_SCREENSHOT_DIR = "${config.home.homeDirectory}/Pictures/Screenshots";
-      };
-    };
-    mime = {
-      enable = true;
-    };
-    mimeApps = {
-      enable = true;
-    };
   };
 }
