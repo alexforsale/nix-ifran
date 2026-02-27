46b9a7e4433fa58d925bb1f048d52e24bce4e90e
alexforsale <alexforsale@yahoo.com>
2026-02-27 22:08:31 +0700

    home-manager/common/ssh: separate file

--- home-manager/common/default.nix
+++ home-manager/common/default.nix
@@ -12,6 +12,7 @@
     ./home-manager
     ./jq
     ./ripgrep
+    ./ssh
     ./tmux
   ];
 
@@ -37,24 +38,6 @@
   };
 
   programs = {
-
-    ssh = {
-      enable = true;
-      enableDefaultConfig = false;
-      matchBlocks."*" = {
-        forwardAgent = false;
-        addKeysToAgent = "no";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
-      };
-    };
-
     zoxide = {
       enable = true;
       enableBashIntegration = true;
