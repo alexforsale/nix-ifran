4d889b249266c956761c396c523bc5f422d060eb
alexforsale <alexforsale@yahoo.com>
2026-02-27 22:11:12 +0700

    home-manager/common/xdg: separate file

--- home-manager/common/default.nix
+++ home-manager/common/default.nix
@@ -1,5 +1,4 @@
 {
-  config,
   inputs,
   pkgs,
   ...
@@ -14,6 +13,7 @@
     ./ripgrep
     ./ssh
     ./tmux
+    ./xdg
     ./zoxide
   ];
 
@@ -35,31 +35,6 @@
     ];
     config = {
       allowUnfree = true;
-    };
-  };
-
-  xdg = {
-    enable = true;
-    userDirs = {
-      enable = true;
-      createDirectories = true;
-      music = "${config.home.homeDirectory}/Music";
-      videos = "${config.home.homeDirectory}/Videos";
-      pictures = "${config.home.homeDirectory}/Pictures";
-      templates = "${config.home.homeDirectory}/Templates";
-      download = "${config.home.homeDirectory}/Downloads";
-      documents = "${config.home.homeDirectory}/Documents";
-      desktop = "${config.home.homeDirectory}/Desktop";
-      publicShare = "${config.home.homeDirectory}/Public";
-      extraConfig = {
-        XDG_SCREENSHOT_DIR = "${config.home.homeDirectory}/Pictures/Screenshots";
-      };
-    };
-    mime = {
-      enable = true;
-    };
-    mimeApps = {
-      enable = true;
     };
   };
 }
---
+++ home-manager/common/xdg/default.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  ...
+}:
+{
+  xdg = {
+    enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+      music = "${config.home.homeDirectory}/Music";
+      videos = "${config.home.homeDirectory}/Videos";
+      pictures = "${config.home.homeDirectory}/Pictures";
+      templates = "${config.home.homeDirectory}/Templates";
+      download = "${config.home.homeDirectory}/Downloads";
+      documents = "${config.home.homeDirectory}/Documents";
+      desktop = "${config.home.homeDirectory}/Desktop";
+      publicShare = "${config.home.homeDirectory}/Public";
+      extraConfig = {
+        XDG_SCREENSHOT_DIR = "${config.home.homeDirectory}/Pictures/Screenshots";
+      };
+    };
+    mime = {
+      enable = true;
+    };
+    mimeApps = {
+      enable = true;
+    };
+  };
+}
