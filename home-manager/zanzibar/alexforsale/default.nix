8016fba9a4014ec51af979d7352a5b7955b56a7b
alexforsale <alexforsale@yahoo.com>
2026-03-04 23:40:07 +0700

    home-manager/zanzibar/alexforsale: import more common settings

--- home-manager/zanzibar/alexforsale/default.nix
+++ home-manager/zanzibar/alexforsale/default.nix
@@ -3,12 +3,22 @@
   ...
 }:
 {
+  imports = [
+    ../../common
+    ../../common/bash
+    ../../common/emacs
+    ../../common/password-store
+  ];
+
   home = {
     username = "alexforsale";
     homeDirectory = "/home/alexforsale";
     stateVersion = "25.11";
     packages = with pkgs; [
       wl-clipboard
+      hunspellDicts.en_US-large
+      hunspellDicts.id_ID
+      nerd-fonts.iosevka
     ];
   };
 }
