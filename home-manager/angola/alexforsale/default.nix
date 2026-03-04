45a0926f374e26be13aac7b9a05cfaa175c8173f
alexforsale <alexforsale@yahoo.com>
2026-03-04 23:39:53 +0700

    home-manager/common/password-store: move pass-git-helper here

--- home-manager/angola/alexforsale/default.nix
+++ home-manager/angola/alexforsale/default.nix
@@ -22,7 +22,6 @@
     homeDirectory = "/home/alexforsale";
     stateVersion = "25.11";
     packages = with pkgs; [
-      pass-git-helper
       hunspell
       hunspellDicts.en_US-large
       hunspellDicts.id_ID
