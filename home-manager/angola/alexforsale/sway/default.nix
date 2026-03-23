0481c4dd8b24c6b5eed482cdfed622d676ce9b78
alexforsale <alexforsale@yahoo.com>
2026-03-24 04:22:58 +0700

    home-manager/angola/alexforsale/sway: set systemd target for swayidle

--- home-manager/angola/alexforsale/sway/default.nix
+++ home-manager/angola/alexforsale/sway/default.nix
@@ -328,11 +328,9 @@ in
           command = display "off";
           resumeCommand = display "on";
         }
-        # {
-        #   timeout = 900;
-        #   command = "${pkgs.systemd}/bin/systemctl suspend";
-        # }
       ];
+
+     systemdTarget = "sway-session.target";
     };
 
     udiskie = {
