4f4984d39c2a6c7f8118688c6bea4ec2184bc7e6
alexforsale <alexforsale@yahoo.com>
2026-03-24 04:23:42 +0700

    home-manager/angola/alexforsale/mail: disable lieer

--- home-manager/angola/alexforsale/mail.nix
+++ home-manager/angola/alexforsale/mail.nix
@@ -54,9 +54,6 @@ in
     };
   };
 
-  programs.lieer.enable = true;
-  services.lieer.enable = true;
-
   accounts = {
     contact = {
       basePath = ".contact";
@@ -148,13 +145,6 @@ in
           smtp = {
             host = "smtp.gmail.com";
             port = 465;
-          };
-          lieer = {
-            enable = true;
-            sync = {
-              enable = true;
-              frequency = "*-*-* *:00:00";
-            };
           };
           mbsync = {
             enable = true;
